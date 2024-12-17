3cd533ddc24322efd9247cdeb8edc1346b002191
tatarose <1814480507@qq.com>
2024-12-17 11:08:19 +0800

    优化修正

--- LRSDFS.py
+++ LRSDFS.py
@@ -1,3 +1,5 @@
+import os
+
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import gaussian_kde
@@ -6,33 +8,46 @@ from sympy.core.random import sample
 
 import LRSDFS_func
 
+if not os.path.exists('plts/convergence_error'):
+    os.makedirs('plts/convergence_error')
+# 如果保存路径不存在则新建路径
+if not os.path.exists('plts/statistics'):
+    os.makedirs('plts/statistics')
+#如果保存路径不存在则新建路径
+if not os.path.exists('plts/dictionary'):
+    os.makedirs('plts/dictionary')
 # 初始化参数
 k1 = 30 #低秩
 k2 = 40 #稀疏
 max_iter = 50  # 最大迭代次数
 tol = 1e-6     # 收敛阈值
+train_file = 'TE_data/train_data/d00.dat'  # 格式化文件名
+train_data = np.loadtxt(train_file)
+if(train_data.shape[0]<train_data.shape[1]):
+        train_data=train_data.T
+train_data = train_data.T
+n_samples, n_features = train_data.shape if train_data.shape[0] > train_data.shape[1] else train_data.shape[::-1]
 
 # 循环处理从 d00 到 d21 的数据
 for i in range(22):  # 遍历 d00 到 d21
-    train_file = 'TE_data/train_data/d00.dat'  # 格式化文件名
+
     #test_file = f'TE_data/test_data/d{}_te.dat'
     test_file = f'TE_data/test_data/d{i:02d}_te.dat'
 
     print(f"Processing Train Data: {train_file}, Test Data: {test_file}")
 
     # 加载训练和测试数据
-    train_data = np.loadtxt(train_file)
+
     test_data = np.loadtxt(test_file)
 
-    if(train_data.shape[0]<train_data.shape[1]):
-        train_data=train_data.T
+
     if(test_data.shape[0]<test_data.shape[1]):
         test_data=test_data.T
     # 数据标准化
-    train_data = train_data.T
+
     test_data = test_data.T
 
-    n_samples, n_features = train_data.shape if train_data.shape[0] > train_data.shape[1] else train_data.shape[::-1]
+
 
     # 初始化字典矩阵 W1 和 W2
     W1 = np.random.normal(loc=50, scale=50, size=(n_samples, k1))
@@ -56,10 +71,6 @@ for i in range(22):  # 遍历 d00 到 d21
     Y1_norms_e = []
     Y2_norms_e = []
 
-    prev_W1 = W1.copy()
-    prev_W2 = W2.copy()
-    prev_Y1 = Y1.copy()
-    prev_Y2 = Y2.copy()
 
     # 迭代更新
     for iteration in range(max_iter):
@@ -85,6 +96,11 @@ for i in range(22):  # 遍历 d00 到 d21
         # 更新 U1 和 U2
         U1, U2 = LRSDFS_func.update_U(Y1=Y1, Y2=Y2)
 
+        prev_W1 = W1.copy()
+        prev_W2 = W2.copy()
+        prev_Y1 = Y1.copy()
+        prev_Y2 = Y2.copy()
+
         # 保存每次的收敛误差
         W1_norms_e.append(np.linalg.norm(W1 - prev_W1))
         W2_norms_e.append(np.linalg.norm(W2 - prev_W2))
@@ -96,34 +112,34 @@ for i in range(22):  # 遍历 d00 到 d21
                 np.linalg.norm(Y1 - prev_Y1) < tol and np.linalg.norm(Y2 - prev_Y2) < tol:
             print("Convergence reached.")
             break
-
-        # 更新上一次的字典矩阵
-        prev_W1 = W1.copy()
-        prev_W2 = W2.copy()
-        prev_Y1 = Y1.copy()
-        prev_Y2 = Y2.copy()
-
     # 计算字典矩阵
     D1 = train_data @ W1
     D2 = train_data @ W2
-    #绘制字典热图
-    plt.imshow(D1, cmap='hot')
-    plt.colorbar()
-    plt.title(f'Dictionary Matrix for {train_file}')
-    plt.show()
-    plt.imshow(D2, cmap='hot')
-    plt.colorbar()
-    plt.title(f'Dictionary Matrix for {train_file}')
-    plt.show()
 
-    #绘制收敛误差折线图
-    plt.plot(W1_norms_e, label='W1')
-    plt.plot(W2_norms_e, label='W2')
-    plt.xlabel('Iterations')
-    plt.ylabel('Convergence Error')
-    plt.legend()
-    plt.title(f'Convergence for {train_file}')
-    plt.show()
+
+
+    #绘制收敛误差折线图,W1在左边，W2在右边
+    fig, axs = plt.subplots(2, 2, figsize=(10, 8))
+    axs[0, 0].plot(W1_norms_e)
+    axs[0, 0].set_xlabel('Iteration')
+    axs[0, 0].set_ylabel('Norm Difference')
+    axs[0, 0].set_title('W1')
+    axs[0, 1].plot(W2_norms_e)
+    axs[0, 1].set_xlabel('Iteration')
+    axs[0, 1].set_ylabel('Norm Difference')
+    axs[0, 1].set_title('W2')
+    axs[1, 0].plot(Y1_norms_e)
+    axs[1, 0].set_xlabel('Iteration')
+    axs[1, 0].set_ylabel('Norm Difference')
+    axs[1, 0].set_title('Y1')
+    axs[1, 1].plot(Y2_norms_e)
+    axs[1, 1].set_xlabel('Iteration')
+    axs[1, 1].set_ylabel('Norm Difference')
+    axs[1, 1].set_title('Y2')
+    fig.tight_layout()
+    #plt.show()
+    # 保存图
+    plt.savefig(f'plts/convergence_error/d{i:02d}.png')
 
     # 计算统计量
     T2_statistics, SPE_statistics = LRSDFS_func.calculate_statistics(X_new=test_data, D1=D1, D2=D2, a=0.5, b=0.5)
@@ -141,20 +157,56 @@ for i in range(22):  # 遍历 d00 到 d21
     kde_SPE = gaussian_kde(SPE_train)
     SPE_limit = np.percentile(SPE_train, alpha * 100)
 
-    # 绘制 T² 统计量和控制限的折线图
-    plt.plot(T2_statistics, label='T2')
-    plt.axhline(y=T2_limit, color='r', linestyle='--', label='Control Limit')
-    plt.xlabel('Sample Index')
-    plt.ylabel('T2 Value')
-    plt.legend()
-    plt.title(f'T² Statistics for {test_file}')
-    plt.show()
+    # # 绘制 T² 统计量和控制限的折线图
+    # plt.plot(T2_statistics, label='T2')
+    # plt.axhline(y=T2_limit, color='r', linestyle='--', label='Control Limit')
+    # plt.xlabel('Sample Index')
+    # plt.ylabel('T2 Value')
+    # plt.legend()
+    # plt.title(f'T² Statistics for {test_file}')
+    # #plt.show()
+    # # 保存图，如果路不存在就新建路径
+    # if not os.path.exists('TE_data/T2_statistics'):
+    #     os.makedirs('TE_data/T2_statistics')
+    # plt.savefig(f'TE_data/T2_statistics/d{i:02d}.png')
+    #
+    # # 绘制 SPE 统计量和控制限的折线图
+    # plt.plot(SPE_statistics, label='SPE')
+    # plt.axhline(y=SPE_limit, color='r', linestyle='--', label='Control Limit')
+    # plt.xlabel('Sample Index')
+    # plt.ylabel('SPE Value')
+    # plt.legend() # 显示图例
+    # plt.title(f'SPE Statistics for {test_file}')
+    # #plt.show()
+    # # 保存图，如果路不存在就新建路径
+    # if not os.path.exists('TE_data/SPE_statistics'):
+    #     os.makedirs('TE_data/SPE_statistics')
+    # plt.savefig(f'TE_data/SPE_statistics/{test_file}.png')
 
-    # 绘制 SPE 统计量和控制限的折线图
-    plt.plot(SPE_statistics, label='SPE')
-    plt.axhline(y=SPE_limit, color='r', linestyle='--', label='Control Limit')
-    plt.xlabel('Sample Index')
-    plt.ylabel('SPE Value')
-    plt.legend() # 显示图例
-    plt.title(f'SPE Statistics for {test_file}')
-    plt.show()
+    #绘制带有控制限的T2统计量和SPE统计量折线图，T2统计量在左边，SPE统计量在右边
+    fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+    axs[0].plot(T2_statistics, label='T2')
+    axs[0].axhline(y=T2_limit, color='r', linestyle='--', label='Control Limit')
+    axs[0].set_xlabel('Sample Index')
+    axs[0].set_ylabel('T2 Value')
+    axs[0].legend()
+    axs[1].plot(SPE_statistics, label='SPE')
+    axs[1].axhline(y=SPE_limit, color='r', linestyle='--', label='Control Limit')
+    axs[1].set_xlabel('Sample Index')
+    axs[1].set_ylabel('SPE Value')
+    axs[1].legend()
+    plt.suptitle(f'Statistics for {test_file}')
+    fig.tight_layout()
+
+    plt.savefig(f'plts/statistics/d{i:02d}.png')
+
+    #绘制字典热图,将两个字典绘制在同一幅图内，再将图保存，
+    plt.subplot(1, 2, 1)
+    plt.imshow(D1, cmap='hot')
+    plt.colorbar()
+    plt.subplot(1, 2, 2)
+    plt.imshow(D2, cmap='hot')
+    plt.colorbar()
+    plt.title(f'Dictionary for {train_file}')
+
+    plt.savefig(f'plts/dictionary/d{i:02d}.png')
